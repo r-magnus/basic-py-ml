11cb2d135c7a7db5ef9381d1b41da4ca15e5b408
r-magnus <ryan.magnuson@outlook.com>
2024-05-16 10:33:02 -0700

    testing wsl relocation (local)

---
+++ scikit-test.py
@@ -0,0 +1,8 @@
+# File for testing beginner's scikit.
+# Ryan Magnuson rmagnuson@westmont.edu
+
+# Setup
+from scikit-learn import datasets
+
+iris = datasets.load_iris()
+digits = datasets.load_digits()
