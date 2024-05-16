cbf7059568edc1d5a0a14afd28d4020137e5bc54
r-magnus <ryan.magnuson@outlook.com>
2024-05-16 11:01:04 -0700

    copied tutorial for scikit

--- scikit-test.py
+++ scikit-test.py
@@ -2,7 +2,20 @@
 # Ryan Magnuson rmagnuson@westmont.edu
 
 # Setup
-from scikit-learn import datasets
+from sklearn import datasets
+import matplotlib.pyplot as plt
 
+# Iris
 iris = datasets.load_iris()
+data = iris.data
+print(data.shape)
+
+# Digits (img ex)
 digits = datasets.load_digits()
+print(digits.images.shape)
+
+print(plt.imshow(digits.images[-1], cmap=plt.cm.gray_r))
+
+# scikit-learn
+data = digits.images.reshape((digits.images.shape[0], -1)) #feature vector 8x8 -> 64
+print(data)
